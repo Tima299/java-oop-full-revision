aae1dcd708922d2cf6ac3360b5e2773b49238f91
Temur <117558961+Tima299@users.noreply.github.com>
2025-03-04 16:14:39 +0500

    the first step

--- src/constructors/Main.java
+++ src/constructors/Main.java
@@ -1,4 +1,15 @@
 package constructors;
 
 public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangleOne = new Rectangle(4, 40);
+        System.out.printf("Area is %.2f%n", rectangleOne.getArea(4, 40));
+        System.out.printf("Perimeter is %.2f%n", rectangleOne.getPerimeter(4, 40));
+        System.out.println(" - ".repeat(15));
+
+        Rectangle rectangleTwo = new Rectangle(3.5, 35.9);
+        System.out.printf("Area is %.2f%n", rectangleTwo.getArea(3.5, 35.9));
+        System.out.printf("Perimeter is %.2f%n", rectangleTwo.getPerimeter(3.5, 35.9));
+        System.out.println(" - ".repeat(15));
+    }
 }
---
+++ src/constructors/Rectangle.java
@@ -0,0 +1,19 @@
+package constructors;
+
+public class Rectangle {
+    public double width;
+    public double height;
+    public Rectangle() {
+        System.out.println("No args constructor is called");
+    }
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    public double getArea(double width, double height) {
+        return (width * height);
+    }
+    public double getPerimeter(double width, double height) {
+        return 2 * (height + width);
+    }
+}
