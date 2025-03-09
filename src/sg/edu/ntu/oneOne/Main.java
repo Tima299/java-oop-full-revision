3c36c6346c78216f13bac521df0a852fe984e175
Temur <117558961+Tima299@users.noreply.github.com>
2025-03-09 10:35:20 +0500

    some changes are added!

---
+++ src/sg/edu/ntu/oneOne/Main.java
@@ -0,0 +1,11 @@
+package sg.edu.ntu.oneOne;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        System.out.printf("The circle has radius of " + c1.getRadius() + " and area of %.2f%n" , c1.getArea());
+
+        Circle c2 = new Circle(2.0);
+        System.out.printf("The circle has radius of %.2f and area of %.2f" , c2.getRadius()  , c2.getArea());
+    }
+}
