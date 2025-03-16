55a8285f132ccb6453ab145ee10b48ae72788a8b
Temur <117558961+Tima299@users.noreply.github.com>
2025-03-16 09:20:34 +0500

    some changes are added!

---
+++ src/sg/edu/ntu/chapterSix/sixOne/Square.java
@@ -0,0 +1,33 @@
+package sg.edu.ntu.chapterSix.sixOne;
+
+public class Square extends Rectangle{
+    public Square() {
+        super();
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(side, side, color, filled);
+    }
+    public double getSide(){
+        return this.width;
+    }
+    public void setSide(double side){
+        this.width = side;
+        this.length = side;
+    }
+    public void setWidth(double side){
+        this.width = side;
+        this.length = side;
+    }
+    public void setLength(double side){
+        this.width = side;
+        this.length = side;
+    }
+    public String toString(){
+        return "Square[" + super.toString() + "]";
+    }
+}
