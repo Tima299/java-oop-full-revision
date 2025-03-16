1997fb6ce69b7e78db8a1503dbdcf34367840b2f
Temur <117558961+Tima299@users.noreply.github.com>
2025-03-16 16:04:36 +0500

    some changes are added!

---
+++ src/tinyProjects/simpleSchoolManagementSystem/version_2/School.java
@@ -0,0 +1,60 @@
+package tinyProjects.simpleSchoolManagementSystem.version_2;
+
+public class School {
+    private Teacher[] teachers;
+    private Student[] students;
+    int studentCount = 0;
+    int teacherCount = 0;
+
+    public School() {
+        this.students = new Student[2];
+        this.teachers = new Teacher[2];
+    }
+
+    public Teacher[] getTeachers() {
+        return teachers;
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public double getTotalMoneyEarned() {
+        double total = 0;
+        for (int i = 0; i < studentCount; i++) {
+            total += students[i].getFeesPaid();
+        }
+        return total;
+    }
+
+    public double getTotalMoneySpent() {
+        double spent = 0;
+        for (int i = 0; i < teacherCount; i++) {
+            spent += teachers[i].getSalaryEarned();
+        }
+        return spent;
+    }
+
+    public void addTeacher(Teacher teacher) {
+        if (teachers.length <= teacherCount) {
+            Teacher[] newTeacher = new Teacher[2 * teachers.length];
+            for (int i = 0; i < teachers.length; i++) {
+                newTeacher[i] = teachers[i];
+            }
+            teachers = newTeacher;
+        }
+        teachers[teacherCount++] = teacher;
+        teacher.setSchool(this);
+    }
+
+    public void addStudent(Student student) {
+        if (students.length <= studentCount) {
+            Student[] newStudent = new Student[2 * students.length];
+            for (int i = 0; i < students.length; i++) {
+                newStudent[i] = students[i];
+            }
+            students = newStudent;
+        }
+        students[studentCount++] = student;
+    }
+}
