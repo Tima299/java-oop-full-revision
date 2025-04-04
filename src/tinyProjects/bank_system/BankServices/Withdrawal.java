03d4d0dfb0a9805a40e1334755c8f6eafa4af579
Temur <117558961+Tima299@users.noreply.github.com>
2025-04-04 18:02:45 +0500

    some changes are added!

--- src/tinyProjects/bank_system/BankServices/Withdrawal.java
+++ src/tinyProjects/bank_system/BankServices/Withdrawal.java
@@ -1,64 +1,14 @@
 package tinyProjects.bank_system.BankServices;
 
-public class Withdrawal {
-    private int date;
-    private double value;
+public class Withdrawal extends Operation {
 
     public Withdrawal(int date, double value) {
-        this.date = date;
-        this.value = value;
+        super(date, value);
     }
 
     public String toString() {
-        return "Withdrawal: " + date + " " + value;
+        return getDate() + "," + getValue() + "-";
     }
-
-    public int getDate() {
-        return date;
-    }
-
-    public double getValue() {
-        return value;
-    }
-
-    public void setDate(int date) {
-        this.date = date;
-    }
-
-    public void setValue(double value) {
-        this.value = value;
-    }
-
-    public boolean equals(Withdrawal withdrawal) {
-        return this.date == withdrawal.date && this.value == withdrawal.value;
-    }
-
-    public int compareTo(Withdrawal withdrawal) {
-        if (this.date < withdrawal.date) {
-            return -1;
-        } else if (this.date > withdrawal.date) {
-            return 1;
-        } else {
-            if (this.value < withdrawal.value) {
-                return -1;
-            } else if (this.value > withdrawal.value) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-    }
-
-    public boolean isDeposit() {
-        return value > 0;
-    }
-
-    public boolean isWithdrawal() {
-        return value < 0;
-    }
-
-    public boolean isTransfer() {
-        return value == 0;
-    }
-
 }
+
+
